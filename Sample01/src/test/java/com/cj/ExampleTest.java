a0c383ba5cdaba992868147260cbf7c018783bb6
kollujurajeshwar <kollujurajeshwar@gmail.com>
2021-06-09 01:00:28 +0530

    Commit and Push

---
+++ Sample01/src/test/java/com/cj/ExampleTest.java
@@ -0,0 +1,18 @@
+package com.cj;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test1() {
+		Assert.fail();
+	}
+	
+	@Test
+	public void test2() {
+		Assert.fail();
+	}
+}
+	     	
